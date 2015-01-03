af07915edce8e8d2616d6ede67c084f8932d68d4
dskkd <dskkd@live.com>
2015-01-04 01:16:24 +0900

    Revised RSpec

---
+++ test.rb
@@ -0,0 +1,6 @@
+person1 = { first: "Daisuke", last: "Kudo" }
+person2 = { first: "Kimiko", last: "Kudo"}
+person3 = { first: "Arata", last: "Kudo"}
+
+params = { father: person1, mather: person2, child: person3 }
+
